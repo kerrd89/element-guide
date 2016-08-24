900a228d3863f1e95de19f02524e74a26c146afc
David Kerr <kerrd89@gmail.com>
2016-08-24 08:16:19 -0600

    initial setup

---
+++ test/index.js
@@ -0,0 +1,11 @@
+'use strict';
+const assert   = require('chai').assert
+const Timer  = require('./unit-tests/timer-test')
+const Pomodoro  = require('./unit-tests/pomodoro-test')
+
+
+describe('our test bundle', function () {
+  it('should work', function () {
+    assert(true);
+  });
+});
